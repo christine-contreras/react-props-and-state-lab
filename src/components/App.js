a396b442ea0322a909ebeb69c05ef29cd80e90ff
Christine Contreras <christineec23@yahoo.com>
2021-06-11 10:59:31 -0500

    lab finished

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,43 @@ class App extends React.Component {
     }
   }
 
+  updateFilters = (event) => {
+    this.setState({
+      filters: {
+        [event.target.name]: event.target.value
+      }
+    })
+  }
+
+  findPets = () => {
+    let url
+    const animal = this.state.filters.type
+
+    if(this.state.filters.type === 'all'){
+      url = '/api/pets'
+    } else {
+      url = `/api/pets?type=${animal}`
+    }
+
+    fetch(url)
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          pets: json
+        })
+      })
+  }
+
+  petAdoped = (petid) => {
+    const newPets = this.state.pets.map(pet => {
+      return pet.id === petid ? {...pet, isAdopted: true } : {...pet}
+    })
+
+    this.setState({
+      pets: newPets
+    })
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +61,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.updateFilters} onFindPetsClick={this.findPets}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.petAdoped}/>
             </div>
           </div>
         </div>
